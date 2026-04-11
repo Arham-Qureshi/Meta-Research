71fef03bcd9a5520aa0917f4a52eae46a9c5f259
Arham-Qureshi <arhamqureshi0098@gmail.com>
2026-04-11 13:03:34 +0530

    added dependecies

--- app.py
+++ app.py
@@ -11,9 +11,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'meta_research.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from extensions import db, login_manager
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
+
+from extensions import db, login_manager, cors, limiter
+
 db.init_app(app)
 login_manager.init_app(app)
+cors.init_app(app, resources={r'/api/*': {'origins': '*'}})
+limiter.init_app(app)
+
+from errors import register_error_handlers
+register_error_handlers(app)
 
 import models
 
--- extensions.py
+++ extensions.py
@@ -1,7 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 db = SQLAlchemy()
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+
+cors = CORS()
+
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=['200 per hour', '50 per minute'],
+    storage_uri='memory://',
+)
